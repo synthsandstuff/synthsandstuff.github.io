bf18438ac20d6b67a7381f2dc1af6b8fdd3bb98a
synthsandstuff <childof03@gmail.com>
2022-07-02 22:36:40 +0100

    Fixed nav to stay above text and added border

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ var box = document.querySelector("body");
 let height = box.clientHeight;
 let width = box.clientWidth;
 
+console.log(width);
+
+function random3DUnitVector()
+{
+    let theta = Math.random() * 2 * Math.PI;
+    let phi = (Math.random() * Math.PI) - 1;
+    return {x: Math.cos(theta)*Math.cos(phi), 
+            y: Math.cos(theta)*Math.sin(phi),
+            z: Math.sin(theta)};
+}
+
 function generate3DPerlin()
 {
     return
