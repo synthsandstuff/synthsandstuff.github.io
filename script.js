52eb9aa97dc88afad961e41ddad9ed9a810b4c05
synthsandstuff <childof03@gmail.com>
2022-07-03 16:13:55 +0100

    Added blog page

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const Ascii_Chars = [' ', '.', '~', '^', '+', '=', ':', ';', '/', '0', '&', '#'];
 
-var box = document.querySelector("body");
+var box = document.querySelector(".foreground");
 let height = box.clientHeight;
 let width = box.clientWidth;
-
-console.log(width);
 
 function random3DUnitVector()
 {
@@ -15,7 +13,44 @@ function random3DUnitVector()
             z: Math.sin(theta)};
 }
 
-function generate3DPerlin()
+function generate3DUnitVectorArray()
 {
-    return
+    let vectorUnitArray = [];
+    let heightinChars = Math.floor(height/6);
+    for(let k = 0; k < 60; k++)
+    {
+        let grid = [];
+        for(let j = 0; j < heightinChars; j++)
+        {
+            let row = [];
+            for(let i = 0; i < 300; i++)
+            {
+                row.push(random3DUnitVector());
+            }
+            grid.push(row);
+        }
+        vectorUnitArray.push(grid);
+    }
+    return vectorUnitArray;
+}
+
+function generatePerlinNoise(x,y)
+{
+    var x0 = Math.floor(x);
+    var x1 = x0 + 1;
+    var y0 = Math.floor(y);
+    var y1 = y0 + 1; 
+
+    var sx = x - x0;
+    var sy = y - y0;
+}
+
+function dot_prod_grid(x, y, vert_x, vert_y){
+    var g_vect = gradients[vert_y][vert_x];
+    var d_vect = {x: x - vert_x, y: y - vert_y};
+    return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
+}
+
+function lin_interp(x, a, b){
+    return a + x * (b-a);
 }
